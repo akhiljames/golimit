cf7d9ec0bafae8ce53a6e66e6afd4d18551b055c
Akhil James <akhiljames@live.com>
2019-05-23 20:16:57 +0530

    concurrency test

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -3,6 +3,7 @@ package golimit_test
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/akhiljames/golimit"
@@ -17,6 +18,7 @@ func (r *Redis) Eval(script string, keys []string, args ...interface{}) (interfa
 	return r.client.Eval(script, keys, args...).Result()
 }
 
+// EvalSha Optimize Lua script execution
 func (r *Redis) EvalSha(sha1 string, keys []string, args ...interface{}) (interface{}, error, bool) {
 	result, err := r.client.EvalSha(sha1, keys, args...).Result()
 	noScript := err != nil && strings.HasPrefix(err.Error(), "NOSCRIPT ")
@@ -43,5 +45,39 @@ func ExampleRateLimiter_Take() {
 		fmt.Println("DROP")
 	}
 	// Output:
+	// PASS
+}
+
+func ExampleTake_concurrency() {
+	concurrency := 5
+	limiter := golimit.NewRateLimiter(
+		&Redis{redis.NewClient(&redis.Options{
+			Addr: "localhost:6379",
+		})},
+		"golimit:concurrency",
+		&golimit.Config{
+			Interval: 1 * time.Second,
+			Capacity: 10,
+		},
+	)
+
+	var wg sync.WaitGroup
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			if ok, _ := limiter.Take(1); ok {
+				fmt.Println("PASS")
+			} else {
+				fmt.Println("DROP")
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	// Output:
+	// PASS
+	// PASS
+	// PASS
+	// PASS
 	// PASS
 }
